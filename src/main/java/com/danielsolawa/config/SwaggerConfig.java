03f4c114e1ab4e83b16638c9f751118f00d91aea
Daniel Solawa <danielsolawa@gmail.com>
2018-01-19 13:05:07 +0100

    added meta data for swagger

--- src/main/java/com/danielsolawa/config/SwaggerConfig.java
+++ src/main/java/com/danielsolawa/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 /**
  * Created by Daniel Solawa on 2018-01-19.
@@ -22,6 +26,22 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData(){
+        Contact contact = new Contact("Daniel Solawa", "http://danielsolawa.com", "danielsolawa@gmail.com");
+
+        return new ApiInfo(
+                "Spring Mvc Rest",
+                "Sample application",
+                "1.0",
+                "Terms of Service:",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>()
+        );
     }
 }
