ff50717e58f281096c13ff283a785ba1a9e8ecf6
Daniel Solawa <danielsolawa@gmail.com>
2018-01-19 11:25:05 +0100

    added configuration for webjars and swagger-ui

---
+++ src/main/java/com/danielsolawa/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.danielsolawa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Daniel Solawa on 2018-01-19.
+ */
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
