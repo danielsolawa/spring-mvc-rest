8a494d4a0eca91fba7f4d4d79afbde87ea5dd89f
Daniel Solawa <danielsolawa@gmail.com>
2018-01-19 13:33:34 +0100

    added api documentation for vendor controller

--- src/main/java/com/danielsolawa/controller/VendorController.java
+++ src/main/java/com/danielsolawa/controller/VendorController.java
@@ -3,6 +3,8 @@ package com.danielsolawa.controller;
 import com.danielsolawa.model.VendorDTO;
 import com.danielsolawa.model.VendorListDTO;
 import com.danielsolawa.service.VendorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
  * Created by Daniel Solawa on 2018-01-18.
  */
 
+@Api(description = "This is a controller for the Vendor model")
 @RestController
 @RequestMapping(VendorController.BASE_URL)
 public class VendorController {
@@ -22,6 +25,7 @@ public class VendorController {
         this.vendorService = vendorService;
     }
 
+    @ApiOperation(value = "Returns a list of all vendors")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public VendorListDTO getVendorList(){
@@ -29,30 +33,36 @@ public class VendorController {
     }
 
 
+    @ApiOperation(value = "Returns a specified vendor by the given id")
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO getVendorById(@PathVariable String id){
         return vendorService.getVendorById(Long.valueOf(id));
     }
 
+
+    @ApiOperation(value = "Creates a new vendor")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public VendorDTO createNewVendor(@RequestBody VendorDTO vendorDTO){
         return vendorService.createNewVendor(vendorDTO);
     }
 
+    @ApiOperation(value = "Updates a vendor, using put method")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO updateVendor(@PathVariable String id, @RequestBody VendorDTO vendorDTO){
         return vendorService.updateVendor(Long.valueOf(id), vendorDTO);
     }
 
+    @ApiOperation(value = "Updates a vendor, using patch method")
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO patchVendor(@PathVariable String id, @RequestBody VendorDTO vendorDTO){
         return vendorService.patchVendor(Long.valueOf(id), vendorDTO);
     }
 
+    @ApiOperation(value = "Deletes a specified vendor by the given id")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteVendorById(@PathVariable String id){
--- src/main/java/com/danielsolawa/model/VendorDTO.java
+++ src/main/java/com/danielsolawa/model/VendorDTO.java
@@ -1,6 +1,7 @@
 package com.danielsolawa.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -9,8 +10,12 @@ import lombok.Data;
 @Data
 public class VendorDTO {
 
+    @ApiModelProperty(value = "an id for the vendor model", required = false)
     private Long id;
+    @ApiModelProperty(value = "a name for the vendor", required = true)
     private String name;
+
+    @ApiModelProperty(value = "a url for the vendor", required = false)
     @JsonProperty("vendor_url")
     private String vendorUrl;
 }
